5cf42957360ef8b6ea67fd8581df4f02e980af67
WandersonMateus <126344728+WandersonMateus@users.noreply.github.com>
2023-12-06 10:35:39 -0300

    add

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function digitacao() {
+    // Substitua 'sua_pagina.html' pelo caminho da sua página de destino
+    window.location.href = './digitacao/digitacao.html';
+}
+function excel() {
+    // Substitua 'sua_pagina.html' pelo caminho da sua página de destino
+    window.location.href = '#Excel';
+}
+function word() {
+    // Substitua 'sua_pagina.html' pelo caminho da sua página de destino
+    window.location.href = '#Word';
+}
+function powerpoint() {
+    // Substitua 'sua_pagina.html' pelo caminho da sua página de destino
+    window.location.href = '#PowerPoint';
+}
