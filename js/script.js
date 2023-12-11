b22a0f896cf341f2c93e409718cb6521fe546a7d
WandersonMateus <126344728+WandersonMateus@users.noreply.github.com>
2023-12-11 00:23:15 -0300

    add

--- js/script.js
+++ js/script.js
@@ -14,3 +14,35 @@ function powerpoint() {
     // Substitua 'sua_pagina.html' pelo caminho da sua página de destino
     window.location.href = '#PowerPoint';
 }
+
+
+function txt01() {
+    window.open('../textos/texto01.html', '_blank');
+}
+function txt02() {
+    window.open('../textos/texto02.html', '_blank');
+}
+function txt03() {
+    window.open('../textos/texto03.html', '_blank');
+}
+function txt04() {
+    window.open('../textos/texto04.html', '_blank');
+}
+function txt05() {
+    window.open('../textos/texto05.html', '_blank');
+}
+function txt06() {
+    window.open('../textos/texto06.html', '_blank');
+}
+function txt07() {
+    window.open('../textos/texto07.html', '_blank');
+}
+function txt08() {
+    window.open('../textos/texto08.html', '_blank');
+}
+function txt09() {
+    window.open('../textos/texto09.html', '_blank');
+}
+function txt10() {
+    window.open('../textos/texto10.html', '_blank');
+}
---
+++ textos/scriptxt.js
@@ -0,0 +1,4 @@
+function txt01() {
+    // Substitua 'sua_pagina.html' pelo caminho da sua página de destino
+    window.location.href = './texto01.html';
+}
