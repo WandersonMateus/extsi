47874c0cbcb6bec964715333daa16b58c05bea8a
WandersonMateus <126344728+WandersonMateus@users.noreply.github.com>
2023-12-11 00:33:24 -0300

    add

--- js/script.js
+++ js/script.js
@@ -1,18 +1,22 @@
 function digitacao() {
-    // Substitua 'sua_pagina.html' pelo caminho da sua página de destino
     window.location.href = './digitacao/digitacao.html';
 }
 function excel() {
-    // Substitua 'sua_pagina.html' pelo caminho da sua página de destino
     window.location.href = '#Excel';
 }
 function word() {
-    // Substitua 'sua_pagina.html' pelo caminho da sua página de destino
     window.location.href = '#Word';
 }
 function powerpoint() {
-    // Substitua 'sua_pagina.html' pelo caminho da sua página de destino
     window.location.href = '#PowerPoint';
+}
+function powerpoint() {
+    window.location.href = '#PowerPoint';
+}
+
+
+function home() {
+    window.location.href = '../index.html';
 }
 
 
